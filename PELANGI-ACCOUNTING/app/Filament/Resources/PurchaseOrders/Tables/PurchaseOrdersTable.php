02d93266222e09cbd7c3760d56b90b725966b279
Silent Solitaire <mohamad.basori12@gmail.com>
2026-06-05 18:24:41 +0700

    fix: add missing Textarea import in PurchaseOrdersTable

--- PELANGI-ACCOUNTING/app/Filament/Resources/PurchaseOrders/Tables/PurchaseOrdersTable.php
+++ PELANGI-ACCOUNTING/app/Filament/Resources/PurchaseOrders/Tables/PurchaseOrdersTable.php
@@ -12,6 +12,7 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Forms\Components\Textarea;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
