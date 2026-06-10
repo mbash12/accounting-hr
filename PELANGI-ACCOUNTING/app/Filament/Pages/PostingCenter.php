62061c3f1ee71be7b097e64933cf48845b742805
Silent Solitaire <mohamad.basori12@gmail.com>
2026-06-10 17:40:24 +0700

    feat: Add HasPageShield trait to PostingCenter for enhanced security

--- PELANGI-ACCOUNTING/app/Filament/Pages/PostingCenter.php
+++ PELANGI-ACCOUNTING/app/Filament/Pages/PostingCenter.php
@@ -3,9 +3,12 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 
 class PostingCenter extends Page
 {
+    use HasPageShield;
+
     protected static ?string $navigationLabel = 'Posting Center';
 
     protected static ?int $navigationSort = 10;
